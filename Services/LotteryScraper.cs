355e74ca476770a77476f93d6888be6157fc894b
Pola35-create <polakishuti@gmail.com>
2025-09-01 15:44:16 +0200

    Added scraping logic for all the remaining lotteries

--- Services/LotteryScraper.cs
+++ Services/LotteryScraper.cs
@@ -18,5 +18,45 @@ namespace Luckerryy.Services
                 return jackpotElement.Text;
             }
         }
+        public string ScrapeHatosLotto()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://bet.szerencsejatek.hu/jatekok/hatoslotto");
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                var jackpotElement = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".expected-price")));
+                return jackpotElement.Text;
+            }
+        }
+        public string ScrapeSkandiLotto()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://bet.szerencsejatek.hu/jatekok/skandinavlotto");
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                var jackpotElement = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".expected-price")));
+                return jackpotElement.Text;
+            }
+        }
+        public string ScrapeEurojackpot()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://bet.szerencsejatek.hu/jatekok/eurojackpot");
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                var jackpotElement = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".expected-price")));
+                return jackpotElement.Text;
+            }
+        }
+        public string ScrapeJoker()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://bet.szerencsejatek.hu/jatekok/joker");
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                var jackpotElement = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".expected-price")));
+                return jackpotElement.Text;
+            }
+        }
     }
 }
