40d1ee79855251bc197cfb276f2ac67f41d981f3
Pola35-create <polakishuti@gmail.com>
2025-09-23 22:23:54 +0200

    Fixed bugs and cleaned up

--- Utils/LotteryUtils.cs
+++ Utils/LotteryUtils.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Linq;
 
 namespace Luckerryy.Utils
@@ -7,20 +8,27 @@ namespace Luckerryy.Utils
     {
         public static decimal ParseJackpot(string jackpotText)
         {
-            var numericText = new string(jackpotText.Where(c => char.IsDigit(c)).ToArray());
+            var numericText = new string(jackpotText
+                .Where(c => char.IsDigit(c) || c == ',')
+                .ToArray());
+
+            numericText = numericText.Replace(',', '.');
+
+            if (!decimal.TryParse(numericText, NumberStyles.Any, CultureInfo.InvariantCulture, out var number))
+            {
+                throw new FormatException($"Could not parse jackpot text: {jackpotText}");
+            }
+
             if (jackpotText.Contains("millió", StringComparison.OrdinalIgnoreCase))
             {
-                return decimal.Parse(numericText) * 1_000_000;
+                return number * 1_000_000;
             }
             if (jackpotText.Contains("milliárd", StringComparison.OrdinalIgnoreCase))
             {
-                return decimal.Parse(numericText) * 100_000_000;
+                return number * 1_000_000_000;
             }
-            return decimal.Parse(numericText);
-        }
-        public static string FormatJackpot(decimal jackpot)
-        {
-            return string.Format("{0:N0} Ft", jackpot).Replace(",", " ");
+
+            return number;
         }
     }
 }
